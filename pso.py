457fb2cd541694941985263a7f9612d4d587e500
Shing Hei Zhan <szhan@Shings-MBP.fritz.box>
2018-02-21 17:54:59 +0100

    Modified to solve the 2-dimensional Griewank problem

--- pso.py
+++ pso.py
@@ -23,17 +23,38 @@ from deap import benchmarks
 from deap import creator
 from deap import tools
 
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Particle", list, fitness=creator.FitnessMax, speed=list, 
+
+test_func = benchmarks.griewank
+test_lb = -100
+test_ub = 100
+test_min_goal = 0
+
+n_dims = 2
+n_inds = 300
+n_gens = 100
+
+p_min = -6
+p_max = 6
+s_min = -3
+s_max = 3
+phi_1 = 2.0
+phi_2 = 2.0
+
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Particle", list, fitness=creator.FitnessMin, speed=list,
     smin=None, smax=None, best=None)
 
+
 def generate(size, pmin, pmax, smin, smax):
-    part = creator.Particle(random.uniform(pmin, pmax) for _ in range(size)) 
+    part = creator.Particle(random.uniform(pmin, pmax) for _ in range(size))
     part.speed = [random.uniform(smin, smax) for _ in range(size)]
     part.smin = smin
     part.smax = smax
     return part
 
+
+# TODO: generalize to n dimensions
 def updateParticle(part, best, phi1, phi2):
     u1 = (random.uniform(0, phi1) for _ in range(len(part)))
     u2 = (random.uniform(0, phi2) for _ in range(len(part)))
@@ -47,44 +68,26 @@ def updateParticle(part, best, phi1, phi2):
             part.speed[i] = part.smax
     part[:] = list(map(operator.add, part, part.speed))
 
+
 toolbox = base.Toolbox()
-toolbox.register("particle", generate, size=2, pmin=-6, pmax=6, smin=-3, smax=3)
+toolbox.register("particle", generate, size=n_dims, pmin=p_min, pmax=p_max, smin=s_min, smax=s_max)
 toolbox.register("population", tools.initRepeat, list, toolbox.particle)
-toolbox.register("update", updateParticle, phi1=2.0, phi2=2.0)
-toolbox.register("evaluate", benchmarks.h1)
+toolbox.register("update", updateParticle, phi1=phi_1, phi2=phi_2)	# TODO: generalize to n dimensions
+toolbox.register("evaluate", test_func)
 
-def main():
-    pop = toolbox.population(n=5)
-    stats = tools.Statistics(lambda ind: ind.fitness.values)
-    stats.register("avg", numpy.mean)
-    stats.register("std", numpy.std)
-    stats.register("min", numpy.min)
-    stats.register("max", numpy.max)
+pop = toolbox.population(n=n_inds)
 
-    logbook = tools.Logbook()
-    logbook.header = ["gen", "evals"] + stats.fields
-
-    GEN = 1000
-    best = None
-
-    for g in range(GEN):
-        for part in pop:
-            part.fitness.values = toolbox.evaluate(part)
-            if not part.best or part.best.fitness < part.fitness:
-                part.best = creator.Particle(part)
-                part.best.fitness.values = part.fitness.values
-            if not best or best.fitness < part.fitness:
-                best = creator.Particle(part)
-                best.fitness.values = part.fitness.values
-        for part in pop:
-            toolbox.update(part, best)
-
-        # Gather all the fitnesses in one list and print the stats
-        logbook.record(gen=g, evals=len(pop), **stats.compile(pop))
-        print(logbook.stream)
-    
-    return pop, logbook, best
-
-if __name__ == "__main__":
-    main()
+best = None
+for g in range(n_gens):
+	for part in pop:
+		part.fitness.values = toolbox.evaluate(part)
+		if not part.best or part.best.fitness < part.fitness:
+			part.best = creator.Particle(part)
+			part.best.fitness.values = part.fitness.values
+		if not best or best.fitness < part.fitness:
+			best = creator.Particle(part)
+			best.fitness.values = part.fitness.values
+	for part in pop:
+		toolbox.update(part, best)
+	# TODO: print or store g, pop, and fitnesses
 
