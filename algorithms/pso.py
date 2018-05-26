50e1ac83d10ec2058ba7d0ecdd6b7df37a24022e
Shing Hei Zhan <szhan@Shings-MacBook-Pro.local>
2018-05-25 19:11:16 -0700

    No longer need to specify the lower or upper bounds for test function

--- algorithms/pso.py
+++ algorithms/pso.py
@@ -2,7 +2,7 @@ import pyswarms as ps
 from pyswarms.utils.functions import single_obj as fx
 
 
-def run_global_best_pso(n_dims, test_func, test_lb, test_ub,\
+def run_global_best_pso(n_dims, test_func,\
 			n_inds, n_gens,\
 			c1=0.5, c2=0.3, w=0.9, k=3, p=2):
 	options = {'c1':c1, 'c2':c2, 'w':w, 'k':k, 'p':p}
@@ -13,8 +13,6 @@ def run_global_best_pso(n_dims, test_func, test_lb, test_ub,\
 
 
 if __name__ == "__main__":
-	run_global_best_pso(n_dims=3,\
-				test_func=fx.ackley_func, test_lb=-5, test_ub=5,\
-				n_inds=5, n_gens=10)
+	run_global_best_pso(n_dims=3, test_func=fx.rastrigin_func, n_inds=5, n_gens=10)
 
 
